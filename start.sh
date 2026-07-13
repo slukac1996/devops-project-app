04516bebd06576cdb761bf1cff2dc0c8c78ed763
slukac1996 <lukac.sara.96@gmail.com>
2026-07-13 18:00:05 +0200

    Containerfile,compose,start skripa -lokalno sve radi

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+NET=ticketing-net
+
+podman run -d --network $NET --name postgres \
+	-e POSTGRES_USER=ticketing_user \
+	-e POSTGRES_PASSWORD=change_me_local \
+	-e POSTGRES_DB=ticketing \
+	docker.io/library/postgres:16
+
+podman run -d --network $NET --name redis \
+docker.io/library/redis:7
+
+echo "Waiting for postgres..."
+sleep 8
+
+podman run -d --network $NET --name api --env-file .env -p 8080:8080 \
+localhost/ticketing-api:dev
+
+podman run -d --network $NET --name worker --env-file .env \
+localhost/ticketing-worker:dev
+
+podman run -d --network $NET --name frontend --env-file .env -p 3000:3000 \
+localhost/ticketing-frontend:dev
+
+echo "Svi pet containera radi"
+
